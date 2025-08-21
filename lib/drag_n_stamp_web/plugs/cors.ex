2df1b176b9fac386147da18f06e4309d4cd5257d
halston <halstonsellentin@gmail.com>
2025-08-20 22:48:22 -0700

    extension path

--- lib/drag_n_stamp_web/plugs/cors.ex
+++ lib/drag_n_stamp_web/plugs/cors.ex
@@ -6,24 +6,32 @@ defmodule DragNStampWeb.Plugs.CORS do
   def call(conn, _opts) do
     # Check if this is an extension request
     user_agent = get_req_header(conn, "user-agent") |> List.first() || ""
-    is_extension = String.contains?(conn.request_path, "mode=extension") or String.contains?(user_agent, "Chrome-Extension")
-    
-    conn = conn
-    |> put_resp_header("access-control-allow-origin", "*")
-    |> put_resp_header("access-control-allow-methods", "GET, POST, OPTIONS")
-    |> put_resp_header("access-control-allow-headers", "content-type")
-    
+
+    is_extension =
+      String.contains?(conn.request_path, "extension") or
+        String.contains?(user_agent, "Chrome-Extension")
+
+    conn =
+      conn
+      |> put_resp_header("access-control-allow-origin", "*")
+      |> put_resp_header("access-control-allow-methods", "GET, POST, OPTIONS")
+      |> put_resp_header("access-control-allow-headers", "content-type")
+
     # Allow iframe embedding for extensions only
-    conn = if is_extension do
-      conn
-      |> put_resp_header("x-frame-options", "ALLOWALL")
-      |> put_resp_header("content-security-policy", "frame-ancestors 'self' chrome-extension: moz-extension:")
-    else
-      conn
-      |> put_resp_header("x-frame-options", "SAMEORIGIN")
-      |> put_resp_header("content-security-policy", "frame-ancestors 'self'")
-    end
-    
+    conn =
+      if is_extension do
+        conn
+        |> put_resp_header("x-frame-options", "ALLOWALL")
+        |> put_resp_header(
+          "content-security-policy",
+          "frame-ancestors 'self' chrome-extension: moz-extension:"
+        )
+      else
+        conn
+        |> put_resp_header("x-frame-options", "SAMEORIGIN")
+        |> put_resp_header("content-security-policy", "frame-ancestors 'self'")
+      end
+
     handle_preflight(conn)
   end
 
--- lib/drag_n_stamp_web/plugs/extension_headers.ex
+++ lib/drag_n_stamp_web/plugs/extension_headers.ex
@@ -5,13 +5,17 @@ defmodule DragNStampWeb.Plugs.ExtensionHeaders do
 
   def call(conn, _opts) do
     # Check if this is an extension request
-    is_extension = String.contains?(conn.request_path, "mode=extension") or 
-                  String.contains?(conn.query_string, "mode=extension")
-    
+    is_extension =
+      String.contains?(conn.request_path, "extension") or
+        String.contains?(conn.query_string, "extension")
+
     if is_extension do
       conn
       |> put_resp_header("x-frame-options", "ALLOWALL")
-      |> put_resp_header("content-security-policy", "frame-ancestors 'self' chrome-extension: moz-extension: chrome-untrusted:")
+      |> put_resp_header(
+        "content-security-policy",
+        "frame-ancestors 'self' chrome-extension: moz-extension: chrome-untrusted:"
+      )
     else
       conn
     end
